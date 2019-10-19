053953f6fae022546c4bb8cd611367117c161898
AListckii <sobbabobba@gmail.com>
2019-10-20 00:39:31 +0400

    feat: partial solution to submit in time

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,47 +2,98 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		let node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
-		
+		if (!this.isEmpty()) {
+			let detached = this.detachRoot();
+			this.restoreRootFromLastInsertedNode(detached);
+			this.shiftNodeDown(this.root);
+			return detached.data;
+		}			
+			
 	}
+
 
 	detachRoot() {
 		
+		if (this.parentNodes.includes(this.root)){
+			this.parentNodes.shift();
+		}
+		let temproot=this.root;
+		this.root = null;
+		return temproot;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+
 	}
 
 	size() {
-		
+
 	}
 
 	isEmpty() {
-		
+		return (!this.root && !this.parentNodes.length)
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		this.parentNodes.push(node);
+		if (!this.root) {
+			this.root = node;
+			return;
+		} else {
+			this.parentNodes[0].appendChild(node);
+		}
+		if (this.parentNodes[0].left && this.parentNodes[0].right) {
+			this.parentNodes.shift();
+		}
+
+
 	}
 
 	shiftNodeUp(node) {
-		
+		if (node.parent) {
+			let nodeIndex = this.parentNodes.indexOf(node);
+			let nodeParentIndex = this.parentNodes.indexOf(node.parent);
+			if (node.priority > node.parent.priority) {
+
+				this.parentNodes[nodeIndex] = node.parent;
+				this.parentNodes[nodeParentIndex] = node;
+			}
+			node.swapWithParent();
+			this.shiftNodeUp(node);
+		} else {
+			this.root = node;
+		}
 	}
 
 	shiftNodeDown(node) {
-		
+		if (!(node) || !(node.left)) {
+			let Child=null;
+
+		if (node.left && node.left.priority > node.priority) {
+			Child = node.left
+			}
+
+		if ((node.right) && (!Child && node.right.priority > node.priority)) {
+				Child = node.right
+				}
+				
+
 	}
 }
 
--- src/node.js
+++ src/node.js
@@ -1,23 +1,90 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (!this.left) {
+			this.left = node;
+		} else if (this.left && this.right) {
+			return
+		} else {
+			this.right = node;
+		}
+		if (node) node.parent = this;
 	}
 
 	removeChild(node) {
-
+		if (node == this.left) {
+			this.left = null;
+			node.parent = null;
+		} else if (node == this.right) {
+			this.right = null;
+			node.parent = null;
+		} else if (node.parent != this) {
+			throw ("node.removeChild");
+		}
 	}
 
 	remove() {
-
+		if (!this.parent) {
+			return;
+		} else {
+			this.parent.removeChild(this)
+		}
 	}
 
 	swapWithParent() {
-		
-	}
+		if (!this.parent) {
+			return;
+		}
+		// связь его c его детьми
+		if (this.right) {
+			this.right.parent = this.parent;
+		}
+		if (this.left) {
+			this.left.parent = this.parent;
+		}                        //+
+		// связь его родителя с другим ребенком
+		if (this == this.parent.right) {   // если он правый
+			if (this.parent.left) {        //   и у родителя есть левый
+				this.parent.left.parent = this;  // то родитель левого он
+			}
+			[this.parent.left, this.left] = [this.left, this.parent.left]; //  меняем местами левый родителя и его левый
+			this.parent.right = this.right;  // правый родителя - его правый
+			this.right = this.parent;   //  его правый - его родитель
+		}
+		if (this == this.parent.left) {    // если он левый
+			if (this.parent.right) {
+				this.parent.right.parent = this;
+			}
+			[this.parent.right, this.right] = [this.right, this.parent.right];
+			this.parent.left = this.left;
+			this.left = this.parent;
+		}                       //+
+		// связь  его с бывшим дедом
+		if (this.parent.parent) {	// если у его родителя есть родитель
+
+			if (this.parent == this.parent.parent.left) {  //  если его родитель левый то левый его родителя теперь он
+				this.parent.parent.left = this;
+			}
+			if (this.parent == this.parent.parent.right) { //правый
+				this.parent.parent.right = this;
+			}
+		}
+		//+
+		// связь его с бывшим родителем
+
+		//this.parent.parent = this; // родитель его бывшего родителя - он
+		//this.parent = this.parent.parent;//  его родитель - родитель его бывшего родител
+
+		[this.parent.parent, this.parent] = [this, this.parent.parent];
+	}					//+
+
 }
 
 module.exports = Node;
